dec978e6cb2491aa690ee9f7b7d5b6a6ce32ef45
BrianBeilby <brianbeilby98@gmail.com>
2024-04-03 19:25:13 -0700

    Changed Main content

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -2,13 +2,32 @@
   import Step from "./Step.svelte";
 
   let steps = [
-    { name: "SakuraBot", icon: "fa-solid fa-robot" },
-    { name: "IEEE Web App", icon: "fa-solid fa-building" },
-    { name: "Pokedex", icon: "fa-solid fa-diagram-project" },
+    {
+      name: "SakuraBot",
+      icon: "fa-solid fa-robot",
+      href: "https://github.com/BrianBeilby/SakuraBot",
+      description:
+        "SakuraBot is a multipurpose Discord bot created with Discord.js, Node.js & MongoDB! SakuraBot has many functionalities including moderation, language translation, and music playing!",
+    },
+    {
+      name: "InventoryManagement",
+      icon: "fa-solid fa-qrcode",
+      href: "https://github.com/vstatnyk/Inventory-tracking-app-CSC190-191",
+      description:
+        "A Full Stack React.js, Node.js + Express.js & Firebase + MongoDB web application built for San Mateo County offering seamless inventory management for their Mosquito & Vector Control District!",
+    },
+    {
+      name: "SacStateIEEE",
+      icon: "fa-solid fa-building",
+      href: "https://sacstateieee.com/",
+      description:
+        "SacStateIEEE is a Next.js & TailwindCSS web application, hosted on Vercel, that offers a user-friendly platform to display information about the IEEE club at Sac State!",
+    },
   ];
 
   let benefits = [
     {
+      metric: "10x",
       name: "a self taught developer",
       description:
         "I taught myself to code using free online resources and absolutely fell in love with the creativity and problem solving that is involved in developing and engineering innovative new online experiences. Starting of with JavaScript, HTML & CSS and evolving my knowledge base to include JavaScript frameworks, backend programming, design, cloud services and much much more.",
@@ -32,99 +51,101 @@
     class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
   >
     <div
-      class="flex flex-col lg:justify-center text-0center lg:text-left gap-6 md:gap-8 lg:gap-10"
+      class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
     >
       <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
         Hi! I'm <span class="poppins text-violet-400">Brian</span> Beilby
-        <br />Full Stack <span class="poppins text-violet-400">Devevloper</span>
+        <br />Full Stack
+        <span class="poppins text-violet-400">Developer</span>
       </h2>
       <p class="text-base sm:text-lg md:text-xl">
-        My <span class="text-violet-400">favorite tech</span> includes JavaScript
-        (NEXT.JS or SvelteKit), TailwindCSS, Node.js + Express.js & PostgreSQL or
-        Firebase/Firestore!
+        My <span class="text-violet-400"> favorite tech</span> includes JavaScript
+        (Next.js or SvelteKit), TailwindCSS, Node.js + Express.js & MongoDB or
+        Firebase!
       </p>
-      <a
-        href="https://www.linkedin.com/in/brian-beilby-7b728923b/"
-        target="_blank"
-        class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer"
+      <button
+        class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
       >
         <div
           class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
         />
         <h4 class="relative z-9">Get in touch &rarr;</h4>
-      </a>
+      </button>
     </div>
     <!-- <div class="relative shadow-2xl grid place-items-center">
       <img
         src={"images/profile.png"}
-        alt="Profile"
+        alt="Zetane Engine"
         class="object-cover z-[2] max-h-[70vh]"
       />
     </div> -->
     <div class="stage-cube-cont">
       <div class="cubespinner">
         <div class="face1">
-          <i class="fa-brands fa-angular" />
+          <i class="fa-brands fa-react" />
         </div>
         <div class="face2">
-          <i class="fa-brands fa-angular" />
+          <i class="fa-brands fa-node-js" />
         </div>
         <div class="face3">
-          <i class="fa-brands fa-angular" />
+          <i class="fa-brands fa-git-alt" />
         </div>
         <div class="face4">
-          <i class="fa-brands fa-angular" />
+          <i class="fa-brands fa-html5" />
         </div>
         <div class="face5">
-          <i class="fa-brands fa-angular" />
+          <i class="fa-brands fa-css3-alt" />
         </div>
         <div class="face6">
-          <i class="fa-brands fa-angular" />
+          <i class="fa-brands fa-python" />
         </div>
       </div>
     </div>
-
   </section>
-  <section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
+  <section class="py-20 lg:py-32 flex flex-col gap-24" id="projects">
     <div class="flex flex-col gap-2 text-center">
-      <h6 class="text-lg sm:text-xl md:text-2xl">
+      <h6 class="text-large sm:text-xl md:text-2xl">
         A few of my creative endeavors.
       </h6>
       <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
         Curious to <span class="poppins text-violet-400">see</span> my work?
       </h3>
     </div>
-    <a
+    <!-- <a
       href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
       target="_blank"
-      class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
+      class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200 z-10"
     >
-      <i class="fa-regular fa-circle-play"></i>
+      <i class="fa-regular fa-circle-play" />
       <p>Watch the video</p>
-    </a>
-    <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
+    </a> -->
+    <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10 z-10">
       <Step step={steps[0]}>
         <p>
-          Smoljames store is a merchandising store created with <strong
-            class="text-violet-400"
-            >Next.js Commerce.js Stripe & Node.js + Express.js!</strong
-          > Commerce.js is a product CMS and Stripe is used for all transaction ahndling.
-        </p>
-      </Step>
-      <Step step={steps[1]}>
-        <p>
-          Smoljames store is a merchandising store created with <strong
-            class="text-violet-400"
-            >Next.js Commerce.js Stripe & Node.js + Express.js!</strong
-          > Commerce.js is a product CMS and Stripe is used for all transaction ahndling.
+          SakuraBot is a multipurpose Discord bot created with <strong
+            class="text-violet-400">Discord.js, Node.js & MongoDB!</strong
+          > SakuraBot has many functionalities including moderation, language translation,
+          and music playing!
         </p>
       </Step>
       <Step step={steps[2]}>
         <p>
-          Smoljames store is a merchandising store created with <strong
-            class="text-violet-400"
-            >Next.js Commerce.js Stripe & Node.js + Express.js!</strong
-          > Commerce.js is a product CMS and Stripe is used for all transaction ahndling.
+          SacStateIEEE is a <strong class="text-violet-400"
+            >Next.js & TailwindCSS</strong
+          >
+          web application, hosted on
+          <strong class="text-violet-400">Vercel</strong>, that offers a
+          user-friendly platform to display information about the IEEE club at
+          Sac State!
+        </p>
+      </Step>
+      <Step step={steps[1]}>
+        <p>
+          A Full Stack <strong class="text-violet-400">React.js</strong>,
+          <strong class="text-violet-400">Node.js + Express.js</strong>
+          & <strong class="text-violet-400">Firebase + MongoDB</strong> web application
+          built for San Mateo County offering seamless inventory management for their
+          Mosquito & Vector Control District!
         </p>
       </Step>
     </div>
@@ -134,9 +155,12 @@
     class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
   >
     <div
+      class="z-[-1] bg-violet-950 w-screen left-1/2 -translate-x-1/2 top-0 h-full absolute"
+    ></div>
+    <div
       class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
     >
-      <h6 class="text-lg sm:text-xl md:text-2xl">Want to know more?</h6>
+      <h6 class="text-large sm:text-xl md:text-2xl">Want to know more?</h6>
       <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
         A bit <span class="poppins text-violet-400">about</span> me.
       </h3>
@@ -146,6 +170,21 @@
     </p>
     <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
       {#each benefits as benefit, index}
+        <!-- <div class="flex flex-col gap-2 mx-auto">
+                  <div class="flex items-end gap-4">
+                      <p
+                          class="poppins text-6xl sm:text-7xl md:text-8xl text-slate-500 font-medium"
+                      >
+                          {benefit.metric}
+                      </p>
+                      <p
+                          class="text-xl sm:text-2xl md:text-3xl capitalize pb-2"
+                      >
+                          {benefit.name}
+                      </p>
+                  </div>
+                  <p class="text-center italic">- {benefit.description}</p>
+              </div> -->
         <div class="flex gap-6 sm:gap-8">
           <p
             class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold"
@@ -161,20 +200,20 @@
         </div>
       {/each}
     </div>
-    <h5 class="text-2xl sm:text-3xl font-semibold text-center poppins">
-      The <span class="text-violet-400 poppins">Complete</span> Package
+    <h5 class={" text-2xl sm:text-3xl font-semibold text-center poppins "}>
+      The <span class="text-violet-400">Complete</span> Package
     </h5>
     <div
       class="flex flex-col overflow-x-scroll gap-10 max-w-[800px] mx-auto w-full"
     >
       <table class="bg-white text-slate-700 rounded text-center">
-        <thead class="border-b border-solid border-slate-200">
+        <thead class={"border-b border-solid border-slate-200  "}>
           <tr class="">
             <th />
             <th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
             <th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
             <th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
-            <th class="whitespace-nowrap bg-violet-700 text-white p-4 px-8"
+            <th class="bg-violet-700 text-white whitespace-nowrap p-4 px-8"
               >Me</th
             >
           </tr>
@@ -182,47 +221,43 @@
         <tbody>
           <tr class="border-b border-solid border-slate-200">
             <td
-              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap"
+              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+              >Dedication</td
             >
-              Dedication
-            </td>
-            <td><i class="fa-solid fa-check text-slate-500"></i></td>
-            <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
-            <td><i class="fa-solid fa-check text-slate-500"></i></td>
-            <td><i class="fa-solid fa-check text-green-500"></i></td>
+            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-green-500" /></td>
           </tr>
           <tr class="border-b border-solid border-slate-200">
             <td
-              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap"
+              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+              >Critical Thought</td
             >
-              Critical Thought
-            </td>
-            <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
-            <td><i class="fa-solid fa-check text-slate-500"></i></td>
-            <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
-            <td><i class="fa-solid fa-check text-green-500"></i></td>
+            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-green-500" /></td>
           </tr>
-          <tr class="border-b border-solid border-slate-200">
+          <tr>
             <td
-              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap"
+              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+              >Interpersonal Skills</td
             >
-              Interpersonal Skills
-            </td>
-            <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
-            <td><i class="fa-solid fa-check text-slate-500"></i></td>
-            <td><i class="fa-solid fa-check text-slate-500"></i></td>
-            <td><i class="fa-solid fa-check text-green-500"></i></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-green-500" /></td>
           </tr>
-          <tr class="border-b border-solid border-slate-200">
+          <tr class="border-t border-solid border-slate-200">
             <td
-              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap"
+              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+              >Progamming Ability</td
             >
-              Programming Ability
-            </td>
-            <td><i class="fa-solid fa-check text-slate-500"></i></td>
-            <td><i class="fa-solid fa-check text-slate-500"></i></td>
-            <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
-            <td><i class="fa-solid fa-check text-green-500"></i></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-green-500" /></td>
           </tr>
         </tbody>
       </table>
