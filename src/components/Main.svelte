6890b1b7ee62e35e1de41948a56da3b43cd16bc4
BrianBeilby <brianbeilby98@gmail.com>
2024-04-04 16:25:41 -0700

    Adjusted z-index of particles background

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -51,7 +51,7 @@
     class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
   >
     <div
-      class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
+      class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10 z-10"
     >
       <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
         Hi! I'm <span class="poppins text-violet-400">Brian</span> Beilby
@@ -63,7 +63,7 @@
         (Next.js or SvelteKit), TailwindCSS, Node.js + Express.js & MongoDB or Firebase!
       </p>
       <button
-        class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
+        class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 z-10"
       >
         <div
           class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
@@ -102,7 +102,7 @@
     </div>
   </section>
   <section class="py-20 lg:py-32 flex flex-col gap-24" id="projects">
-    <div class="flex flex-col gap-2 text-center">
+    <div class="flex flex-col gap-2 text-center z-10">
       <h6 class="text-large sm:text-xl md:text-2xl">
         A few of my creative endeavors.
       </h6>
