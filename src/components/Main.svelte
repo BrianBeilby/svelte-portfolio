1fb6fcf44d63cdb532339e8ce77ee56a4f053562
BrianBeilby <brianbeilby98@gmail.com>
2024-04-04 12:01:27 -0700

    Changed content

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -28,7 +28,7 @@
   let benefits = [
     {
       metric: "10x",
-      name: "a self taught developer",
+      name: "a passionate",
       description:
         "I taught myself to code using free online resources and absolutely fell in love with the creativity and problem solving that is involved in developing and engineering innovative new online experiences. Starting of with JavaScript, HTML & CSS and evolving my knowledge base to include JavaScript frameworks, backend programming, design, cloud services and much much more.",
     },
@@ -60,8 +60,7 @@
       </h2>
       <p class="text-base sm:text-lg md:text-xl">
         My <span class="text-violet-400"> favorite tech</span> includes JavaScript
-        (Next.js or SvelteKit), TailwindCSS, Node.js + Express.js & MongoDB or
-        Firebase!
+        (Next.js or SvelteKit), TailwindCSS, Node.js + Express.js & MongoDB or Firebase!
       </p>
       <button
         class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
@@ -79,7 +78,7 @@
         class="object-cover z-[2] max-h-[70vh]"
       />
     </div> -->
-    <div class="stage-cube-cont">
+    <div class="stage-cube-cont hidden sm:flex">
       <div class="cubespinner">
         <div class="face1">
           <i class="fa-brands fa-react" />
@@ -155,9 +154,6 @@
     class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
   >
     <div
-      class="z-[-1] bg-violet-950 w-screen left-1/2 -translate-x-1/2 top-0 h-full absolute"
-    ></div>
-    <div
       class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
     >
       <h6 class="text-large sm:text-xl md:text-2xl">Want to know more?</h6>
@@ -165,106 +161,25 @@
         A bit <span class="poppins text-violet-400">about</span> me.
       </h3>
     </div>
-    <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">
-      I am . . .
-    </p>
-    <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
-      {#each benefits as benefit, index}
-        <!-- <div class="flex flex-col gap-2 mx-auto">
-                  <div class="flex items-end gap-4">
-                      <p
-                          class="poppins text-6xl sm:text-7xl md:text-8xl text-slate-500 font-medium"
-                      >
-                          {benefit.metric}
-                      </p>
-                      <p
-                          class="text-xl sm:text-2xl md:text-3xl capitalize pb-2"
-                      >
-                          {benefit.name}
-                      </p>
-                  </div>
-                  <p class="text-center italic">- {benefit.description}</p>
-              </div> -->
-        <div class="flex gap-6 sm:gap-8">
-          <p
-            class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold"
-          >
-            0{index + 1}
-          </p>
-          <div class="flex flex-col gap-6 sm:gap-8">
-            <h3 class="text-2xl sm:text-3xl md:text-5xl">
-              {benefit.name}
-            </h3>
-            <p>{benefit.description}</p>
-          </div>
-        </div>
-      {/each}
-    </div>
-    <h5 class={" text-2xl sm:text-3xl font-semibold text-center poppins "}>
-      The <span class="text-violet-400">Complete</span> Package
-    </h5>
-    <div
-      class="flex flex-col overflow-x-scroll gap-10 max-w-[800px] mx-auto w-full"
+    <p
+      class="mx-auto poppins text-center text-slate-200 font-semibold text-lg sm:text-xl md:text-2xl"
     >
-      <table class="bg-white text-slate-700 rounded text-center">
-        <thead class={"border-b border-solid border-slate-200  "}>
-          <tr class="">
-            <th />
-            <th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
-            <th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
-            <th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
-            <th class="bg-violet-700 text-white whitespace-nowrap p-4 px-8"
-              >Me</th
-            >
-          </tr>
-        </thead>
-        <tbody>
-          <tr class="border-b border-solid border-slate-200">
-            <td
-              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
-              >Dedication</td
-            >
-            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
-            <td><i class="fa-solid fa-check text-slate-500" /></td>
-            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
-            <td><i class="fa-solid fa-check text-green-500" /></td>
-          </tr>
-          <tr class="border-b border-solid border-slate-200">
-            <td
-              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
-              >Critical Thought</td
-            >
-            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
-            <td><i class="fa-solid fa-check text-slate-500" /></td>
-            <td><i class="fa-solid fa-check text-slate-500" /></td>
-            <td><i class="fa-solid fa-check text-green-500" /></td>
-          </tr>
-          <tr>
-            <td
-              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
-              >Interpersonal Skills</td
-            >
-            <td><i class="fa-solid fa-check text-slate-500" /></td>
-            <td><i class="fa-solid fa-check text-slate-500" /></td>
-            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
-            <td><i class="fa-solid fa-check text-green-500" /></td>
-          </tr>
-          <tr class="border-t border-solid border-slate-200">
-            <td
-              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
-              >Progamming Ability</td
-            >
-            <td><i class="fa-solid fa-check text-slate-500" /></td>
-            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
-            <td><i class="fa-solid fa-check text-slate-500" /></td>
-            <td><i class="fa-solid fa-check text-green-500" /></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+      I am currently a Computer Science student at <span
+        class="poppins text-violet-400"
+        >California State University, Sacramento</span
+      > set to graduate this semester. My experience spans many different types of
+      projects from web applications to command line applications. Outside of coding,
+      I am an avid weightlifter and music enthusiast.
+    </p>
+    <p
+      class="mx-auto poppins text-center text-slate-300 text:md sm:text:lg md:text-xl"
+    >
+      As I transition to the professional world, I am eager to bring my blend of
+      creativity and technical expertise to a team passionate about delivering
+      impactful software solutions.
+    </p>
     <div class="mx-auto -mt-12 italic sm:hidden opacity-50">
       <p>Scroll to see more &rarr;</p>
     </div>
-    <p class="mx-auto">So why not invest?</p>
   </section>
 </main>
