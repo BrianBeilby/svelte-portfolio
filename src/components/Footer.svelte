066d829175dfe1d3b13932a8ff7e8f6b4e5653a6
BrianBeilby <brianbeilby98@gmail.com>
2024-04-04 16:25:47 -0700

    Update Footer.svelte

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,42 +1,33 @@
 <footer
-  class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
+  class="py-6 sm:py-8 bg-transparent border-t border-solid border-violet-950 flex flex-col gap-2 sm:gap-4 justify-center items-center z-10"
 >
-  <p class="px-4 py-2 bg-white text-slate-950 font-medium">
-    Connect with me &darr;
+  <p>
+    <a
+      href="mailto:brianbeilby98@gmail.com"
+      class="text-violet-400 hover:text-violet-300"
+      >brianbeilby98@gmail.com<sup
+        ><span class="text-xs scale-75 pl-0.5">
+          <i
+            class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+          />
+        </span></sup
+      ></a
+    >
   </p>
-  <div class="flex flex-col gap-4 items-center justify-center">
-    <p><b class="pr-2">Email</b> brianbeilby98@gmail.com</p>
+  <div class="flex flex-wrap gap-4 items-center justify-center">
     <p>
-      <b class="pr-2">Github</b>
-      <a
-        class="text-violet-400"
-        target="_blank"
-        href="https://github.com/BrianBeilby"
-      >
-        BrianBeilby<sup
-          ><span class="text-xs scale-75 pl-0.5">
-            <i
-              class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
-            />
-          </span></sup
-        >
+      <a target="_blank" href="https://github.com/BrianBeilby">
+        <i class="fa-brands fa-github text-2xl"></i>
       </a>
     </p>
     <p>
-        <b class="pr-2">LinkedIn</b>
-        <a
-          class="text-violet-400"
-          target="_blank"
-          href="https://www.linkedin.com/in/brian-beilby-7b728923b/"
-        >
-          BrianBeilby<sup
-            ><span class="text-xs scale-75 pl-0.5">
-              <i
-                class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
-              />
-            </span></sup
-          >
-        </a>
-      </p>
+      <a
+        class="text-sky-600"
+        target="_blank"
+        href="https://www.linkedin.com/in/brian-beilby-7b728923b/"
+      >
+        <i class="fa-brands fa-linkedin text-2xl"></i>
+      </a>
+    </p>
   </div>
 </footer>
